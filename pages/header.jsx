110da5224b0aa0e53f56ed05cee09e6abbd6a556
NamikoToriyama <namiko.trym@gmail.com>
2021-11-15 02:02:26 +0900

    ヘッダーの修正
    Signed-off-by: NamikoToriyama <namiko.trym@gmail.com>

--- pages/header.jsx
+++ pages/header.jsx
@@ -4,7 +4,7 @@ export default function TearaNavbar() {
     return (
       <Navbar bg="light" expand="lg">
           <Container>
-            <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+            <Navbar.Brand href="#home">TEAra Website</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav className="me-auto">
