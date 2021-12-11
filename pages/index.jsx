cc36cdfb6b442df157233ccca42f82cec46dbc86
si-tm <kumomaya@gmail.com>
2021-12-11 10:47:24 +0900

    スクロールボタン追加

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,6 +2,12 @@ import Head from 'next/head'
 import TearaNavbar from './header'
 import TearaFooter from './footer'
 import { Container } from 'react-bootstrap'
+import {Link as Scroll} from "react-scroll"
+import About from './components/about'
+import Activities from './components/activities'
+import Contact from './components/contact'
+import Members from './components/members'
+import Navbutton from './components/navbutton'
 
 export default function Home() {
   return (
@@ -19,6 +25,26 @@ export default function Home() {
           Get started by editing <code>pages/index.js</code>
         </p>
       </Container>
+      
+      <header>
+        <nav>
+          <Navbutton index="about"/>
+          <Navbutton index="activities"/>
+          <Navbutton index="members"/>
+          <Navbutton index="contact"/>
+        </nav>
+      </header>
+
+      <About/>
+      <section id = "about"/>
+      <Activities/>
+      <section id = "activities"/>
+      <Members/>
+      <section id = "members"/>
+      <Contact/>
+      <section id = "contact"/>
+      
+
       <TearaFooter />
     </Container>
   )
