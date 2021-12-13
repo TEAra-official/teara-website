0766f259c4be0ec289f014802894e1ec74604b82
konamilk <konamishu-stu@yahoo.co.jp>
2021-12-13 20:28:07 +0900

    フッターの追加

--- pages/footer.jsx
+++ pages/footer.jsx
@@ -1,13 +1,17 @@
 export default function TearaFooter(){
-    return(
-    <footer className="cntr-footer">
-    <a
-      href="https://vercel.com?filter=next.js&utm_source=github&utm_medium=example&utm_campaign=next-example"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      Powered by{' '}
-      <img src="/vercel.svg" alt="Vercel Logo" className="sml-logo" />
-    </a>
+  return(
+  <footer className="footer-background">
+    <div className="filter">
+
+        <a href="https://twitter.com/teara_ocha" target="_blank">  
+          <img className="twitter-logo" src="/Twitter_logo_white.png" alt="Twitterロゴ" />
+        </a>
+
+        <a href="https://github.com/TEAra-official" target="_blank">  
+          <img className="github-logo" src="/Github-logo-white.png" alt="Githubロゴ" />
+        </a> 
+        <div className="copyright">© 2021 TEAra</div>
+
+    </div>
   </footer>)
 }
