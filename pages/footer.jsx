6760651b05efdd0a7829d7eeed8a3365848bad9c
konamilk <konamishu-stu@yahoo.co.jp>
2021-12-24 22:55:25 +0900

    フッターのロゴでImageタグを使うようにした

--- pages/footer.jsx
+++ pages/footer.jsx
@@ -1,16 +1,23 @@
+import Image from 'next/image'
+
 export default function TearaFooter(){
   return(
   <footer className="footer-background">
     <div className="filter">
 
-        <a href="https://twitter.com/teara_ocha" target="_blank">  
-          <img className="twitter-logo" src="/Twitter_logo_white.png" alt="Twitterロゴ" />
+      <div className="twitter-and-github-logo">
+        <a  href="https://twitter.com/teara_ocha" target="_blank">  
+          <Image src="/images/Twitter-logo-white.png" width={30} height={30} objectFit="contain" alt="Twitterロゴ" />
         </a>
+      </div>
 
-        <a href="https://github.com/TEAra-official" target="_blank">  
-          <img className="github-logo" src="/Github-logo-white.png" alt="Githubロゴ" />
-        </a> 
-        <div className="copyright">© 2021 TEAra</div>
+      <div className="twitter-and-github-logo">
+        <a  href="https://twitter.com/teara_ocha" target="_blank">  
+          <Image src="/images/Github-logo-white.png" width={30} height={30} objectFit="contain" alt="Githubロゴ" />
+        </a>
+      </div>
+
+      <div className="copyright">© 2021 TEAra</div>
 
     </div>
   </footer>)
