c84e74e97cb43ad37c60dc681bb6011bcd685ca7
konamilk <konamishu-stu@yahoo.co.jp>
2021-12-30 17:17:03 +0900

    githubアイコンのリンク先を修正

--- pages/footer.jsx
+++ pages/footer.jsx
@@ -10,7 +10,7 @@ export default function TearaFooter(){
         </a>
       </div>
       <div className="twitter-and-github-logo">
-        <a  href="https://twitter.com/teara_ocha" target="_blank">  
+        <a  href="https://github.com/TEAra-official" target="_blank">  
           <Image src="/images/Github-logo-white.png" width={30} height={30} objectFit="contain" alt="Githubロゴ" />
         </a>
       </div>
