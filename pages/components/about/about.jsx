12c7576426aacc44bbcc180459d7045f3494b16f
konamilk <konamishu-stu@yahoo.co.jp>
2022-01-03 12:05:38 +0900

    「詳しく見る」というボタンを押すと折り畳み要素が開く

--- pages/components/about/about.jsx
+++ pages/components/about/about.jsx
@@ -1,19 +1,34 @@
-import MainButton from '/pages/components/button';
+import useCollapse from 'react-collapsed'
+import { Nav } from 'react-bootstrap'
+import { Link as Scroll } from "react-scroll"
 
 export default function About() {
+  const { getCollapseProps, getToggleProps, isExpanded } = useCollapse()
+
   return (
-      <div className="about">
-        <h1>About</h1>
-        <div className="about-content">
-          <p>あああああああああああああ
-            <br/>
-            あああああああああああああああああああああ
+    <>
+      <h2 id="about" className="about-title">ABOUT</h2>
+      <div className="about-content">
+        <p>
+          TEAraはお茶の水女子大学の開発コミュニティです。
+          <br />
+          あああああああああああああああああああああああああああああああ
+          <p {...getCollapseProps()}>
+            あああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああ
+            <br />
+            ああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああ
+            <br />
           </p>
-        </div>
-          <MainButton
-            href="/components/about/about-detail"
-            content="詳しく見る"
-          />
+        </p>
+        <Nav.Link>
+          <Scroll to="about" smooth={true} duration={600} offset={-100}>
+            <div className="toggle" {...getToggleProps()}>
+              {isExpanded ? '戻す' : '詳しく見る'}
+            </div>
+          </Scroll>
+        </Nav.Link>
+
       </div>
+    </>
   )
 }
