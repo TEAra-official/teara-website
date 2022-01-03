0eabde5d2c4f620ac9f551c6702b77658930ae7c
konamilk <konamishu-stu@yahoo.co.jp>
2022-01-03 12:04:38 +0900

    トップのタイトルの編集

--- pages/components/main-title.jsx
+++ pages/components/main-title.jsx
@@ -1,7 +1,7 @@
 export default function Title() {
     return ( 
         <div className="main-title">
-            <h1>TEAra</h1>
+            <font size="7">TEAra</font>
             <h5>お茶大開発コミュニティ</h5>
         </div>
     )
