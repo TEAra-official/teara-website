a0f5aa334a83bdcf90cb9eed064b8fdc79e9e45c
si-tm <kumomaya@gmail.com>
2022-01-27 18:54:07 +0900

    contact fetchの変更

--- pages/components/contact.jsx
+++ pages/components/contact.jsx
@@ -1,11 +1,96 @@
-import { Container } from 'react-bootstrap'
+import { useState } from "react";
+import type { GetServerSideProps } from 'next';
+import Link from 'next/link';
+import useSWR from 'swr';
+import * as React from 'react';
+import { fetchPosts } from '../../_lib/posts';
 
-export default function Contact() {
-    return (
-        <body>
-            <h1>
-                Contact
-            </h1>
-        </body>
-    )
-}  
+
+export default function Home() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [submitted, setSubmitted] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("送信中");
+
+    let data = {
+      name,
+      email,
+      message,
+    };
+
+    // fetch("../api/contact", {
+    //   method: "POST",
+    //   headers: {
+    //     Accept: "application/json, text/plain, */*",
+    //     "Content-Type": "application/json",
+    //   },
+    //   body: JSON.stringify(data),
+    // }).then((res) => {
+    //   if (res.status === 200) {
+    //     console.log("送信が成功しました");
+    //     setSubmitted(true);
+    //     setName("");
+    //     setEmail("");
+    //     setMessage("");
+    //   }
+    // });
+
+    async function fetcher(url: "../api/contact"): Promise<boolean | null> {
+      const response = await fetch(url);
+      return response.json();
+    }
+    const { data: isLoggedIn } = useSWR('/api/user', fetcher);
+
+  };
+
+  return (
+    <div>
+      <form>
+        <formGroup>
+          <label htmlFor="name">名前</label>
+          <input
+            type="text"
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
+            name="name"
+          />
+        </formGroup>
+
+        <formGroup>
+          <label htmlFor="email">メールアドレス</label>
+          <input
+            type="email"
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+            name="email"
+          />
+        </formGroup>
+
+        <formGroup>
+          <label htmlFor="message">メッセージ</label>
+          <input
+            type="text"
+            onChange={(e) => {
+              setMessage(e.target.value);
+            }}
+            name="message"
+          />
+        </formGroup>
+
+        <input
+          type="submit"
+          onClick={(e) => {
+            handleSubmit(e);
+          }}
+        />
+      </form>
+    </div>
+  );
+}
+
