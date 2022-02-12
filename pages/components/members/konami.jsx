30fbc31206f4b89bf23a79d9b725a38186c2b2c5
konamilk09 <83947560+konamilk09@users.noreply.github.com>
2022-02-12 19:31:18 +0900

    membersの作成( (#25)
    * membersの個人ページのボタンを追加
    
    * membersの見た目の調整
    
    * ボタンのファイルを減らし、関数名の修正して、画像のsrcをフォルダ内にした
    
    Co-authored-by: konamilk <konamishu27@yahoo.co.jp>

---
+++ pages/components/members/konami.jsx
@@ -0,0 +1,28 @@
+import TearaFooter from '../../footer'
+import { LeftButton, RightButton } from './button'
+import PageTitle from '../page-title'
+import Image from 'next/image'
+
+export default function Konami() {
+  return (
+    <div>
+      <PageTitle title="MEMBER" />
+      <div className="member-content">
+        <Image className="circle" src="/images/members-icon/konami.jpg"
+          width={180} height={180} />
+        <div className="member-detail">
+          <p className="member-name">konamilk</p>
+          <p>#競プロ #CTF</p>
+          <p>情報系でつよつよになりたい新B2</p>
+        </div>
+      </div>
+      <div className="member-button">
+        <LeftButton href="/components/members/leader" />
+        <RightButton
+          href="/components/members/konami2"
+        />
+      </div>
+      <TearaFooter />
+    </div>
+  )
+}
---
+++ pages/components/members/konami2.jsx
@@ -0,0 +1,26 @@
+import TearaFooter from '../../footer'
+import { LeftButton, RightButton } from './button'
+import PageTitle from '../page-title'
+import Image from 'next/image'
+
+export default function Konami2() {
+  return (
+    <div>
+      <PageTitle title="MEMBER" />
+      <div className="member-content">
+        <Image className="circle" src="/images/members-icon/icon-basecolor.png"
+          width={180} height={180} />
+        <div className="member-detail">
+          <p className="member-name">konamilk2</p>
+          <p>自己紹介</p>
+        </div>
+      </div>
+      <div className="member-button">
+        <LeftButton href="/components/members/konami" />
+        <RightButton
+          href="/components/members/last" />
+      </div>
+      <TearaFooter />
+    </div>
+  )
+}
