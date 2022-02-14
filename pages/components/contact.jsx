7e07ff82281631bccb83615029cf180487181d45
si-tm <kumomaya@gmail.com>
2022-02-14 21:54:11 +0900

    contact 文言追加

--- pages/components/contact.jsx
+++ pages/components/contact.jsx
@@ -1,101 +1,19 @@
-import { useState } from "react";
-// import type { GetServerSideProps } from 'next';
-import Link from 'next/link';
-import useSWR from 'swr';
-import * as React from 'react';
-// import { fetchPosts } from '../../_lib/posts';
-import contact from "../api/contact";
+import useCollapse from 'react-collapsed'
+import { Nav } from 'react-bootstrap'
+import { Link as Scroll } from "react-scroll"
 
-
-export default function Home() {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [message, setMessage] = useState("");
-  const [submitted, setSubmitted] = useState(false);
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log("送信中");
-
-    let data = {
-      name,
-      email,
-      message,
-    };
-
-    // fetch("../api/contact", {
-    //   method: "POST",
-    //   headers: {
-    //     Accept: "application/json, text/plain, */*",
-    //     "Content-Type": "application/json",
-    //   },
-    //   body: JSON.stringify(data),
-    // }).then((res) => {
-    //   if (res.status === 200) {
-    //     console.log("送信が成功しました");
-    //     setSubmitted(true);
-    //     setName("");
-    //     setEmail("");
-    //     setMessage("");
-    //   }
-    // });
-
-    async function fetcher(url) {
-      const response = await fetch(url);
-      return response.json();
-    }
-    // async function fetcher(url): Promise<boolean | null> {
-    //   const response = await fetch(url);
-    //   return response.json();
-    // }
-    const { data: isLoggedIn } = useSWR('/api/user', fetcher);
-
-  };
+export default function Conatct() {
 
   return (
-    <div>
-      <form>
-        <formGroup>
-          <label htmlFor="name">名前</label>
-          <input
-            type="text"
-            onChange={(e) => {
-              setName(e.target.value);
-            }}
-            name="name"
-          />
-        </formGroup>
-
-        <formGroup>
-          <label htmlFor="email">メールアドレス</label>
-          <input
-            type="email"
-            onChange={(e) => {
-              setEmail(e.target.value);
-            }}
-            name="email"
-          />
-        </formGroup>
-
-        <formGroup>
-          <label htmlFor="message">メッセージ</label>
-          <input
-            type="text"
-            onChange={(e) => {
-              setMessage(e.target.value);
-            }}
-            name="message"
-          />
-        </formGroup>
-
-        <input
-          type="submit"
-          onClick={(e) => {
-            handleSubmit(e);
-          }}
-        />
-      </form>
+    <div className="contact-content">
+      <h2 id="contact" className="contact-title">CONTACT</h2>
+      <p>
+      お問い合わせにつきましては、TEAraのTwitter(
+        <a href="https://twitter.com/teara_ocha">
+        @teara_ocha
+        </a>
+        )DMまたは、teara.offical[atmark]gmail.comまで連絡お願いします。
+      </p>
     </div>
-  );
+  )
 }
-
