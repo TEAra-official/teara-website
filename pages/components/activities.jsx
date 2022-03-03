ab6349a3494435288efbb634698a49d620a69863
si-tm <kumomaya@gmail.com>
2022-03-03 23:25:33 +0900

    日付の書式の修正

--- pages/components/activities.jsx
+++ pages/components/activities.jsx
@@ -28,12 +28,12 @@ export default function Activities() {
                 <div className="sub">
                     <h1>Activities</h1>
                 </div>
-                <Activities_column date = "2021/6/25" subject = "TEAra立ち上げ" summery = "活動を開始しました"/>
-                <Activities_column date = "2021/7/15" subject = "自分のwebサイトをつくってみよう" summery = "webサイトの概要を学んだあとHTMLをいじったり、firebaseを使ってwebサイトをホスティングしたりしました。"/>
-                <Activities_column date = "2021/8/6" subject = "gitゆるゆる勉強会" summery = "gitをあまり使ったことない人対象にgitについてのワークショップを行いました。"/>
-                <Activities_column date = "2021/9/13" subject = "HTMLで作成したフォームをサーバーサイドに送ってみよう" summery = "サーバーサイド作成の説明を受けたあと実装しました。"/>
-                <Activities_column date = "2021/9/26" subject = "LT大会" summery = "初の内部LT大会を行いました。さまざまなテーマの発表があって面白かったです。"/>
-                <Activities_column date = "2021/11/~" subject = "社内資料勉強会" summery = "サイボウズさんの新人研修資料を教材として勉強会を行なっています"/>
+                <Activities_column date = "2021 / 6 / 25" subject = "TEAra立ち上げ" summery = "活動を開始しました"/>
+                <Activities_column date = "2021 / 7 / 15" subject = "自分のwebサイトをつくってみよう" summery = "webサイトの概要を学んだあとHTMLをいじったり、firebaseを使ってwebサイトをホスティングしたりしました。"/>
+                <Activities_column date = "2021 / 8 / 6" subject = "gitゆるゆる勉強会" summery = "gitをあまり使ったことない人対象にgitについてのワークショップを行いました。"/>
+                <Activities_column date = "2021 / 9 / 13" subject = "HTMLで作成したフォームをサーバーサイドに送ってみよう" summery = "サーバーサイド作成の説明を受けたあと実装しました。"/>
+                <Activities_column date = "2021 / 9 / 26" subject = "LT大会" summery = "初の内部LT大会を行いました。さまざまなテーマの発表があって面白かったです。"/>
+                <Activities_column date = "2021 / 11 / ~" subject = "社内資料勉強会" summery = "サイボウズさんの新人研修資料を教材として勉強会を行なっています"/>
             </ul>
         </div>
     )
