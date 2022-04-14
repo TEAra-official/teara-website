ead79cebd9a603988321928f482f2419c2b60e64
si-tm <kumomaya@gmail.com>
2022-04-14 16:54:40 +0900

    unify css

--- pages/components/activities.jsx
+++ pages/components/activities.jsx
@@ -27,19 +27,15 @@ export default function Activities() {
     return (
       <div className="activity-background">
             <ul className="list-group list-group-flush">
-            <link href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:wght@700&display=swap" rel="stylesheet"/>
-                <div className="title">
-                 <h2 id="activities" className="title">ACTIVITIES</h2>
-                </div>
-                <div className="plain-text-japanese">
-                  {/* <link href="https://fonts.googleapis.com/css2?family=Kiwi+Maru:wght@400;500&family=M+PLUS+Rounded+1c&family=Reggae+One&family=Roboto+Serif:ital,wght@0,500;0,700;1,600&display=swap" rel="stylesheet"></link> */}
-                  <Activities_column date = "2021 / 06 / 25" subject = "TEAra立ち上げ" summery = "活動を開始しました"/>
-                  <Activities_column date = "2021 / 07 / 15" subject = "自分のwebサイトをつくってみよう" summery = "webサイトの概要を学んだあとHTMLをいじったり、firebaseを使ってwebサイトをホスティングしたりしました。"/>
-                  <Activities_column date = "2021 / 08 / 06" subject = "gitゆるゆる勉強会" summery = "gitをあまり使ったことない人対象にgitについてのワークショップを行いました。"/>
-                  <Activities_column date = "2021 / 09 / 13" subject = "HTMLで作成したフォームをサーバーサイドに送ってみよう" summery = "サーバーサイド作成の説明を受けたあと実装しました。"/>
-                  <Activities_column date = "2021 / 09 / 26" subject = "LT大会" summery = "初の内部LT大会を行いました。さまざまなテーマの発表があって面白かったです。"/>
-                  <Activities_column date = "2021 / 11 / ~" subject = "社内資料勉強会" summery = "サイボウズさんの新人研修資料を教材として勉強会を行なっています"/>
-                </div>
+              <h2 id="activities" className="title">ACTIVITIES</h2>
+              <div className="plain-text-japanese">
+                <Activities_column date = "2021 / 06 / 25" subject = "TEAra立ち上げ" summery = "活動を開始しました"/>
+                <Activities_column date = "2021 / 07 / 15" subject = "自分のwebサイトをつくってみよう" summery = "webサイトの概要を学んだあとHTMLをいじったり、firebaseを使ってwebサイトをホスティングしたりしました。"/>
+                <Activities_column date = "2021 / 08 / 06" subject = "gitゆるゆる勉強会" summery = "gitをあまり使ったことない人対象にgitについてのワークショップを行いました。"/>
+                <Activities_column date = "2021 / 09 / 13" subject = "HTMLで作成したフォームをサーバーサイドに送ってみよう" summery = "サーバーサイド作成の説明を受けたあと実装しました。"/>
+                <Activities_column date = "2021 / 09 / 26" subject = "LT大会" summery = "初の内部LT大会を行いました。さまざまなテーマの発表があって面白かったです。"/>
+                <Activities_column date = "2021 / 11 / ~" subject = "社内資料勉強会" summery = "サイボウズさんの新人研修資料を教材として勉強会を行なっています"/>
+              </div>
             </ul>
         </div>
     )
