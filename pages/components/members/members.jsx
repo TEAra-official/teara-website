bb90dfa86edc73fd2546fd5b367d25e84df8850b
konamilk <konamishu27@yahoo.co.jp>
2022-04-20 19:17:17 +0900

    フォントの統一

--- pages/components/members/members.jsx
+++ pages/components/members/members.jsx
@@ -6,9 +6,9 @@ export default function Members() {
   const { getCollapseProps, getToggleProps, isExpanded } = useCollapse()
 
   return (
-    <div className="members-content">
-      <h2 id="members" className="about-title">MEMBERS</h2>
-      <p className="members-text"> 大学1年生から修士2年までの約40名で活動しています</p>
+    <div className="members-content plain-text-japanese">
+      <h2 id="members" className="about-title title">MEMBERS</h2>
+      <p className="members-text plain-text-japanese"> 大学1年生から修士2年までの約40名で活動しています</p>
       <div>
         <div className="members">
           <Link href='./components/members/tori'>
@@ -28,6 +28,19 @@ export default function Members() {
             <a className="members-icon">
               <Image className="circle" src="/images/members-icon/konami.jpg"
                 width={150} height={150} alt="konamiのアイコン" />
+            </a>
+          </Link>
+          <Link href='./components/members/pai'>
+            <a className="members-icon">
+              <Image className="circle" src="/images/members-icon/basecolor.png"
+                width={150} height={150} alt="paiのアイコン" />
+              <p className="tag1">代表</p>
+            </a>
+          </Link>
+          <Link href='./components/members/ocha'>
+            <a className="members-icon">
+              <Image className="circle" src="/images/members-icon/basecolor.png"
+                width={150} height={150} alt="ochaのアイコン" />
             </a>
           </Link>
         </div>
